d1daebf8d5ea7771a61f70c05ab5b35d9d00a2cc
mark g <mark.goldstein@avanade.com>
2018-01-25 16:02:46 -0800

    about, vimeo done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+// import { HomeComponent } from './home/home.component';
+import { InstagramComponent } from './instagram/instagram.component';
+import { VimeoComponent } from './vimeo/vimeo.component';
+
+const routes: Routes = [
+  { path: 'about', loadChildren: 'app/home/home.module#HomeModule' },
+  { path: '', redirectTo: 'about', pathMatch: 'full'},
+  { path: 'instagram', component: InstagramComponent },
+  { path: 'vimeo', component: VimeoComponent },
+  // { path: '**', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [InstagramComponent, VimeoComponent] 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
+import { VimeoService } from './vimeo/vimeo.service';
+import { InstagramService } from './instagram/instagram.service';
+
+import { Globals } from '../../globals';
+import { VimeoPlayerComponent } from './vimeo/vimeo-player.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    routingComponents,
+    NavBarComponent,
+    FooterComponent,
+    VimeoPlayerComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpModule,
   ],
-  providers: [],
+  providers: [Globals, VimeoService, InstagramService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
