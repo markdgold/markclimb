3c5bfb90083f5c6edbcc4929ad0bd0d4f00e24a8
mark g <mark.goldstein@avanade.com>
2018-03-29 16:11:48 -0700

    fix for ig modal sizing on mobile

--- src/app/instagram/insta-modal.component.ts
+++ src/app/instagram/insta-modal.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
 import { DomSanitizer } from '@angular/platform-browser';
 
 declare var $:any;
@@ -13,6 +12,7 @@ export class InstaModalComponent implements OnInit{
   embedUrl: string = "https://www.instagram.com/p/Bd9Q-FWAFw_/";
   active: boolean;
   imageData: any;
+  isMobileFlag: boolean = false;
 
   constructor(private sanitizer: DomSanitizer) { }
 
@@ -33,20 +33,32 @@ export class InstaModalComponent implements OnInit{
     return this.sanitizer.bypassSecurityTrustResourceUrl(this.embedUrl+"embed/?cr=1&v=8&wp=987#%7B%22ci%22%3A0%2C%22os%22%3A954.235%7D");
   }
   iframeWidth(){
+    if (parseInt($('.modal-container').css('width'))< 500){
+      this.isMobileFlag = true;
+    } else {
+      this.isMobileFlag = false;
+    }
     let imageHeight = parseInt(this.imageData.images.standard_resolution.height);
     let imageWidth = parseInt(this.imageData.images.standard_resolution.width);
-    return (imageWidth > imageHeight) ? imageWidth :  '500';
+    if (this.isMobileFlag) {
+      return parseInt($('.modal-container').css('width'));
+    }
+    return (imageWidth > imageHeight) ? imageWidth :  '410';
   }
 
   iframeHeight(){
     let imageHeight = parseInt(this.imageData.images.standard_resolution.height);
     let imageWidth = parseInt(this.imageData.images.standard_resolution.width);
-    let windowHeight = parseInt($('.modal-container').css('height'))    
+    let windowHeight = parseInt($('.modal-container').css('height'));    
+    let windowWidth = parseInt($('.modal-container').css('width'));    
     let headerHeight = 54;
     let textHeight = 40+24+44;
     let iFrameHeight = imageHeight+headerHeight+textHeight;
     if (imageHeight === imageWidth) return 500+headerHeight+textHeight;
-    return (iFrameHeight < 788)? iFrameHeight : '788' ;
+    if (this.isMobileFlag) {
+      return (imageHeight/imageWidth*windowWidth)+headerHeight+textHeight;
+    }
+    return (iFrameHeight < 788)? iFrameHeight : '678' ;
   }
 
   ngOnInit() {  }
