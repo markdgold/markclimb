db23592968885145b70a4ab55385be2b02aba945
mark g <mark.goldstein@avanade.com>
2018-03-30 15:53:13 -0700

    ig iframe more fix

--- src/app/instagram/insta-modal.component.ts
+++ src/app/instagram/insta-modal.component.ts
@@ -12,17 +12,35 @@ export class InstaModalComponent implements OnInit{
   embedUrl: string = "https://www.instagram.com/p/Bd9Q-FWAFw_/";
   active: boolean;
   imageData: any;
-  isMobileFlag: boolean = false;
+  imageWidth: number;
+  imageHeight: number;
+  imageOrientation: string;
+  isMobileFlag: boolean;
 
   constructor(private sanitizer: DomSanitizer) { }
 
   openModal(imageData){
-    this.imageData = imageData;
-    this.embedUrl = imageData.link;
     this.active = true;
     $('body').css('position','fixed').css('overflow-y','scroll'); //fix this
+    this.imageData = imageData;
+    this.embedUrl = imageData.link;
+    console.log(imageData);
+    this.imageHeight = parseInt(this.imageData.images.standard_resolution.height);
+    this.imageWidth = parseInt(this.imageData.images.standard_resolution.width);
+    this.setImageOrientation(this.imageHeight, this.imageWidth);
   }
   
+  setImageOrientation(imageHeight, imageWidth){
+    switch (imageWidth < imageHeight ){
+      case false:
+        this.imageOrientation = 'h';
+        break;
+      case true:
+        this.imageOrientation = 'v';
+        break;
+    }
+  }
+
   closeModal(){
     this.active = false;
     this.embedUrl = "https://www.instagram.com/p/Bd9Q-FWAFw_/"
@@ -38,27 +56,31 @@ export class InstaModalComponent implements OnInit{
     } else {
       this.isMobileFlag = false;
     }
-    let imageHeight = parseInt(this.imageData.images.standard_resolution.height);
-    let imageWidth = parseInt(this.imageData.images.standard_resolution.width);
     if (this.isMobileFlag) {
+      if (this.imageOrientation === 'v'){ return parseInt($('.modal-container').css('width'))-100}
       return parseInt($('.modal-container').css('width'));
     }
-    return (imageWidth > imageHeight) ? imageWidth :  '410';
+    return (this.imageOrientation === 'h') ? this.imageWidth :  '508';
   }
 
   iframeHeight(){
-    let imageHeight = parseInt(this.imageData.images.standard_resolution.height);
-    let imageWidth = parseInt(this.imageData.images.standard_resolution.width);
     let windowHeight = parseInt($('.modal-container').css('height'));    
     let windowWidth = parseInt($('.modal-container').css('width'));    
     let headerHeight = 54;
     let textHeight = 40+24+44;
-    let iFrameHeight = imageHeight+headerHeight+textHeight;
-    if (imageHeight === imageWidth) return 500+headerHeight+textHeight;
+    let iFrameHeight = this.imageHeight+headerHeight+textHeight;
     if (this.isMobileFlag) {
-      return (imageHeight/imageWidth*windowWidth)+headerHeight+textHeight;
+      if (this.imageOrientation === 'v'){
+        return (this.imageHeight/this.imageWidth*(windowWidth-100))+headerHeight+textHeight;
+      }
+      return (this.imageHeight/this.imageWidth*windowWidth)+headerHeight+textHeight;
     }
-    return (iFrameHeight < 788)? iFrameHeight : '678' ;
+
+    return (this.imageOrientation === 'h') ? iFrameHeight : this.imageHeight/this.imageWidth*508+headerHeight+textHeight;
+
+
+    // if (this.imageHeight === this.imageWidth) return 500+headerHeight+textHeight;
+    // return (iFrameHeight < 788)? iFrameHeight : '678' ;
   }
 
   ngOnInit() {  }
